2ef5f987eca9f09116311c66e7a5f530c29f8450
ZeroDimen <rlaehdrjs951@naver.com>
2025-08-20 17:38:29 +0900

    Add Player Find Game
    2025.08.20
    -- AI중 플레이어를 찾는 게임 제작
    -- PhotonNetwork에서 제공하는 IsMasterClient를 사용하여 동기화된 NPC 생성
    -- 플레이어가 패배했을 때만 플레이어들의 위치를 보여주기 위해 Culling Mask를 사용한 이름 가리기 및 옵저버 카메라 추가

---
+++ Assets/02. Scripts/Find/Find_Billboard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Find_Billboard : MonoBehaviour
+{
+    public Transform mainCamera;
+
+    void Awake()
+    {
+        mainCamera = Camera.main.transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+}
---
+++ Assets/02. Scripts/Find/Find_HitboxEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+public class Find_HitboxEvent : MonoBehaviour
+{
+    private PhotonView myPv;
+
+    void Awake()
+    {
+        myPv = transform.root.GetComponent<PhotonView>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("NPC"))
+        {
+            other.GetComponent<Find_AgentController>().GetHit();
+        }
+        else if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Find_PlayerController>().GetHit();
+
+            if (myPv.IsMine)
+            {
+                bool isWinner = Find_GameManager.Instance.SetScore();
+
+                if (isWinner)
+                {
+                    string nickName = myPv.Owner.NickName;
+                    myPv.RPC("Winner", RpcTarget.AllBuffered, nickName);
+                }
+            }
+        }
+    }
+}
